5f812db180c1ee7c93c077a5495830e5a66118c5
Mandula Naresh Babu <naresh2223@gmail.com>
2019-11-01 22:14:37 -0400

    Datatypes
    01-Nov-2019: Naresh : Datatypes in Java

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,30 @@
+package basics;
+
+public class DataTypes {
+
+		
+	public static void main(String[] args) {
+			
+		// Primitive datatypes
+		int a =10;
+		float b=12.66f;
+		boolean c= true;
+		boolean d= false;
+		double e = 123.234324;
+		char f = 'g';
+		long l = 213234;
+		byte j = 127;
+		short s = 2334;
+		
+		System.out.println("Integer value of a is : " + a);
+		System.out.println("Float value of b is : " + b);
+		System.out.println("Boolean value of c is : " + c);
+		System.out.println("Boolean value of d is : " + d);
+		System.out.println("Double value of e is : " + e);
+		System.out.println("Char value of f is : " + f);
+		System.out.println("Long value of l is : " + l);
+		System.out.println("Byte value of j is : " + j);
+		System.out.println("Byte value of s is : " + s);
+	}
+
+}
