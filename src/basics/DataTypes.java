451f64dd4b15190b052a8459d21cc594ca219c81
Mandula Naresh Babu <naresh2223@gmail.com>
2019-11-01 22:41:22 -0400

    01-Nov-2019:Naresh: Updated Datatypes.java file with comments

--- src/basics/DataTypes.java
+++ src/basics/DataTypes.java
@@ -15,11 +15,11 @@ public class DataTypes {
 		long l = 213234;
 		byte j = 127;
 		short s = 2334;
-		
+		System.out.println("Below are primitive datatypes..!");
 		System.out.println("Integer value of a is : " + a);
 		System.out.println("Float value of b is : " + b);
 		System.out.println("Boolean value of c is : " + c);
-		System.out.println("Boolean value of d is : " + d);
+		System.out.println("Boolean datatype value of d is : " + d);
 		System.out.println("Double value of e is : " + e);
 		System.out.println("Char value of f is : " + f);
 		System.out.println("Long value of l is : " + l);
